4e94e5da20d4f129e1c54d212989f080e52ab534
Сардор Сафаров <skhsafarov@edu.hse.ru>
2021-01-18 02:52:04 +0500

    02:50 18.01.2021

---
+++ Module_01/Contest_03/TaskH/bin/Debug/netcoreapp3.1/code.cs
@@ -0,0 +1,16 @@
+using System;
+// this is my first program
+public class Program
+{
+    static void Main(string[] args)
+    {
+        /* this line print "Hello, world!" */
+        Console.WriteLine("Hello, world!");
+        /*
+         *  
+         *   
+         *    
+         */   
+    }
+}
+// Good luck
