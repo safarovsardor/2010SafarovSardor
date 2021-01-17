e837dfa14a25c3367922a3cce2a149f77f061ffe
Сардор Сафаров <skhsafarov@edu.hse.ru>
2021-01-18 02:52:04 +0500

    02:50 18.01.2021

---
+++ Module_03/Notepad+/Notepad+/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Text File(*.txt|*.txt)";
+        }
+
+
+        private void menuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
